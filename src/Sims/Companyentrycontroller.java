d805850465cefe98ec816bf47e1bf8cb773e046f
Amal C S <amal4cs@gmail.com>
2014-12-24 14:55:52 +0530

    Company Database is update on company data entry

--- src/Sims/Companyentrycontroller.java
+++ src/Sims/Companyentrycontroller.java
@@ -6,7 +6,11 @@
 package Sims;
 
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -27,7 +31,22 @@ public class Companyentrycontroller implements Initializable{
     @FXML private TextField cst;
 
     @FXML private void add(ActionEvent ev){
-        
+        try {
+            String stmt = "INSERT INTO COMPANY (Name,Address,Place,Phone,DL,TIN,CST) "
+                    + "VALUES(?,?,?,?,?,?,?)";
+            PreparedStatement updatecompany = Database.GetPreparedStmt(stmt);
+            updatecompany.setString(1,name.getText());
+            updatecompany.setString(2,area.getText());
+            updatecompany.setString(3,place.getText());
+            updatecompany.setString(4, phone.getText());
+            updatecompany.setString(5, dl.getText());
+            updatecompany.setString(6, tin.getText());
+            updatecompany.setString(7, cst.getText());
+            int executeUpdate = updatecompany.executeUpdate();
+            System.out.println(executeUpdate+" Row Changed");
+        } catch (SQLException ex) {
+            Logger.getLogger(Companyentrycontroller.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
