1fce7a25ce24765fec0e78662f5cff1e798439b8
Amal C S <amal4cs@gmail.com>
2015-01-26 21:01:46 +0530

    Implemente method to show current orderno in purchaseorder

--- src/Sims/PurchaseOrderController.java
+++ src/Sims/PurchaseOrderController.java
@@ -6,21 +6,26 @@
 package Sims;
 
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
 
 /**
  *
  * @author amalcs
  */
-public class PurchaseOrderController implements Initializable{
+public class PurchaseOrderController implements Initializable {
+
     @FXML
     Text fxaddress;
     @FXML
@@ -32,9 +37,28 @@ public class PurchaseOrderController implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         DateFormat d = new SimpleDateFormat("dd-MM-yyyy");
         Date date = new Date();
         fxdate.setText(d.format(date));
+        try {
+            String stmnt = "SELECT MAX(Orderno) FROM PURCHASE";
+            PreparedStatement GetPreparedStmt = Database.GetPreparedStmt(stmnt);
+            ResultSet executeQuery = GetPreparedStmt.executeQuery();
+            int val=0;
+            if (executeQuery.next()) {
+                String s = executeQuery.getString(1);
+                if (s == null) {
+                    val = 0;
+                } else {
+                    val = Integer.parseInt(s);
+                }
+                val++;
+            }
+            fxorderno.setText(val + "");
+        } catch (SQLException ex) {
+            Logger.getLogger(PurchaseOrderController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
